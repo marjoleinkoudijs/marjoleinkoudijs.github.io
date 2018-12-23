99eb0b64827f05d76a3b1a5bbd5adc5504b9cb74
Marjolein Koudijs <koudijs.marjolein@gmail.com>
2018-12-23 12:09:39 +0100

    Commit demo version 0.0

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+	// Load page
+	$('#page').fadeIn(1500);
+
+  	// Navbar (transparant to solid when scrolling down/up)
+    $(window).scroll(function() {
+      	if($(this).scrollTop() > 60) { 
+          	$('.navbar').addClass('solid');
+      	} else {
+          	$('.navbar').removeClass('solid');
+      	}
+    });
+
+    // Navbar (smooth scrolling)
+    var scrollLink = $('.scroll');
+	scrollLink.click(function(e) {
+		e.preventDefault();
+	    $('body,html').animate({
+	    	scrollTop: $(this.hash).offset().top - 56
+	    }, 750 );
+
+	    // close menu pending on width < 768px
+	    if (window.matchMedia("(max-width: 768px)").matches) {
+			$('.navbar-toggler').click();
+		}
+	});
+
+    // Footer (icon color-witch when hovering)
+    $('#footer a').hover(function(){
+    	$(this).find('img').toggleClass('iconHover')
+    });
+
+});
